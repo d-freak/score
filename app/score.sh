6e97929153b8c82af6d2219adfbce7346ab918f1
clavicula <clavicula@d-freak.com>
2017-12-28 15:10:29 +0900

    スコアリセットに対応

--- app/score.sh
+++ app/score.sh
@@ -6,13 +6,19 @@ readonly SAVE_DIR_PATH=$SCRIPT_DIR_PATH/.domino
 export SAVE_DATA_PATH=$SAVE_DIR_PATH/score
 export INPUT=$*
 
-mkdir -p $SAVE_DIR_PATH 2>/dev/null
-
 if [ $# -eq 0 ]; then
     echo $SAVE_DATA_PATH
     cat $SAVE_DATA_PATH
     exit 0
 fi
+
+if [ $1 = "--reset" ]; then
+    mv -f $SAVE_DATA_PATH $SAVE_DATA_PATH.bak 2>/dev/null
+    echo "done."
+    exit 0
+fi
+
+mkdir -p $SAVE_DIR_PATH 2>/dev/null
 
 node $SCRIPT_DIR_PATH/score.js
 
